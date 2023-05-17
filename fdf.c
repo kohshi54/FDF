fef56576b26e99fbe0d23197cf0b5ca825e07008
Kohshi Yamaguchi <kyamaguc@c1r15s3.42tokyo.jp>
2023-05-17 18:38:07 +0900

    brasenham

--- fdf.c
+++ fdf.c
@@ -10,12 +10,57 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
+}
+
+#define	DX(a, b) (a > b ? 1 : (-1))
+#define	DY(a, b) (a > b ? 1 : (-1))
+
+#include <math.h>
+void	line_put(t_data *data, double x0, double y0, double x1, double y1, int color)
+{
+	double	delta_x;
+	double	delta_y;
+
+	delta_x = x1 - x0;
+	delta_y = y1 - y0;
+
+	int	dx;
+	int	dy;
+
+	dx = DX(x1, x0);
+	dy = DY(y1, y0);
+
+	double	x;
+	double	y;
+
+	x = x0;
+	y = y0;
+	if (delta_x > delta_y)
+	{
+		while (fabs(x - x1) > 1)
+		{
+			if (delta_y != 0)
+				y = y + (delta_y / delta_x) * dy;
+			x = x + dx;
+			pixel_put(data, x, y, color);
+		}
+	}
+	else
+	{
+		while (fabs(y - y1) > 1)
+		{
+			if (delta_x != 0)
+				x = x + (delta_x / delta_y) * dx;
+			y = y + dy;
+			pixel_put(data, x, y, color);
+		}
+	}
 }
 
 void	draw_box(t_data *data, size_t x, size_t y, int color)
@@ -26,15 +71,15 @@ void	draw_box(t_data *data, size_t x, size_t y, int color)
 	i = 10;
 	while (i < x)
 	{
-		my_mlx_pixel_put(data, i, 10, color);
-		my_mlx_pixel_put(data, 10, i, color);
+		pixel_put(data, i, 10, color);
+		pixel_put(data, 10, i, color);
 		i++;
 	}
 	j = 10;
 	while (j < y)
 	{
-		my_mlx_pixel_put(data, i, j, color);
-		my_mlx_pixel_put(data, j, i, color);
+		pixel_put(data, i, j, color);
+		pixel_put(data, j, i, color);
 		j++;
 	}
 }
@@ -49,14 +94,14 @@ void	draw_triagngle(t_data *data, size_t x, size_t y, int color)
 	x++;
 	while (i < 100)
 	{
-		my_mlx_pixel_put(data, 200 + i, 10 + i, color);
-		my_mlx_pixel_put(data, 200 - i, 10 + i, color);
+		pixel_put(data, 200 + i, 10 + i, color);
+		pixel_put(data, 200 - i, 10 + i, color);
 		i++;
 	}
 	j = 200 - i;
 	while (j < 200 + i)
 	{
-		my_mlx_pixel_put(data, j, 10 + i, color);
+		pixel_put(data, j, 10 + i, color);
 		j++;
 	}
 }
@@ -73,9 +118,12 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
-	draw_box(&img, 100, 100, 0x00FF0000);
-	draw_triagngle(&img, 100, 100, 0x00FF0000);
+	line_put(&img, 10, 10, 300, 100, 0x00FF0000);
+	line_put(&img, 100, 100, 300, 100, 0x00FF0000);
+	line_put(&img, 100, 100, 10, 10, 0x00FF0000);
+	line_put(&img, 10, 10, 100, 300, 0x00FF0000);
+	line_put(&img, 100, 100, 100, 500, 0x00FF0000);
+	line_put(&img, 300, 100, 100, 500, 0x00FF0000);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-
 	mlx_loop(mlx);
 }
