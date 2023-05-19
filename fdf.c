0647de5e06de338f94fbc7f0c301c515376eacd9
Kohshi Yamaguchi <kyamaguc@c1r16s8.42tokyo.jp>
2023-05-19 18:30:49 +0900

    cube

--- fdf.c
+++ fdf.c
@@ -1,6 +1,7 @@
 #include "fdf.h"
 #include <mlx.h>
 #include <unistd.h>
+#include <math.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -10,7 +11,7 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void	pixel_put(t_data *data, int x, int y, int color)
+void	put_pixel(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -18,34 +19,32 @@ void	pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-#include <math.h>
-#include <stdio.h>
-
 typedef	struct line_info
 {
-	int	dx;
-	int	dy;
-	int	x;
-	int	y;
-	int	deltaA;
-	int	deltaB;	
-	int	nabla;
+	int		dx;
+	int		dy;
+	int		x;
+	int		y;
+	size_t	deltaA;
+	size_t	deltaB;
+	int		nabla;
 }	t_line_info	;
 
 void	put_line(t_data *data, int x0, int y0, int x1, int y1, int color)
 {
 	t_line_info	info;
-	int	i;
+	size_t		i;
 
 	if (x0 < x1) info.dx = 1; else info.dx = -1;
 	if (y0 < y1) info.dy = 1; else info.dy = -1;
 
 	info.x = x0;
 	info.y = y0;
-	pixel_put(data, info.x, info.y, color);
+	put_pixel(data, info.x, info.y, color);
 
 	info.deltaA = fabs((float)(x1 - x0));
 	info.deltaB = fabs((float)(y1 - y0));
+
 	if (info.deltaA > info.deltaB)
 	{
 		info.nabla = 2 * info.deltaB - info.deltaA;
@@ -62,7 +61,7 @@ void	put_line(t_data *data, int x0, int y0, int x1, int y1, int color)
 				info.nabla = info.nabla + 2 * info.deltaB;
 			}
 			info.x = info.x + info.dx;
-			pixel_put(data, info.x, info.y, color);
+			put_pixel(data, info.x, info.y, color);
 		}
 	}
 	else
@@ -81,7 +80,7 @@ void	put_line(t_data *data, int x0, int y0, int x1, int y1, int color)
 				info.nabla = info.nabla + 2 * info.deltaA;
 			}
 			info.y = info.y + info.dy;
-			pixel_put(data, info.x, info.y, color);
+			put_pixel(data, info.x, info.y, color);
 		}
 	}
 }
@@ -111,6 +110,16 @@ int	main(void)
 	// put_line(&img, 10, 10, 100, 10, 0x00FF0000);
 	// put_line(&img, 10, 100, 200, 10, 0x00FF0000);
 	// put_line(&img, 10, 100, 100, 10, 0x00FF0000);
+	put_line(&img, 300, 300, 300, 400, 0x00FF0000);
+	put_line(&img, 300, 300, 400, 300, 0x00FF0000);
+	put_line(&img, 300, 300, 400, 400, 0x00FF0000);
+	put_line(&img, 300, 400, 400, 400, 0x00FF0000);
+	put_line(&img, 400, 300, 400, 400, 0x00FF0000);
+	put_line(&img, 300, 300, 350, 250, 0x00FF0000);
+	put_line(&img, 400, 300, 450, 250, 0x00FF0000);
+	put_line(&img, 350, 250, 450, 250, 0x00FF0000);
+	put_line(&img, 450, 250, 450, 350, 0x00FF0000);
+	put_line(&img, 400, 400, 450, 350, 0x00FF0000);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
