18d0b589c623f25fe760c51a23b46adcb9d2cbcc
Kohshi Yamaguchi <kyamaguc@c1r16s8.42tokyo.jp>
2023-05-22 13:46:42 +0900

    struct define and set at the same time

--- fdf.c
+++ fdf.c
@@ -30,7 +30,6 @@ t_coordinate	rotate_z(t_coordinate	coordinate, double radian)
 	return (new);
 }
 
-// t_coordinate	translate(t_coordinate	coordinate, double radian)
 t_coordinate	translate(t_coordinate	coordinate)
 {
 	t_coordinate	new;
@@ -46,26 +45,16 @@ t_coordinate	translate(t_coordinate	coordinate)
 void	put_axis(t_data img)
 {
 	// 座標軸
-	t_coordinate	x_axis;
-	x_axis.x = X_WIDTH / 3 - 50;
-	x_axis.y = 0;
-	x_axis.z = 0;
-	t_coordinate	y_axis;
-	y_axis.x = 0;
-	y_axis.y = Y_WIDTH / 2;
-	y_axis.z = 0;
-	t_coordinate	z_axis;
-	z_axis.x = 0;
-	z_axis.y = -(Y_WIDTH / 3);
-	z_axis.z = 0;
-	t_coordinate	origin;
-	origin.x = 0;
-	origin.y = 0;
-	origin.z = 0;
+	t_coordinate	x_axis = {X_WIDTH / 3 - 50, 0, 0};
+	t_coordinate	y_axis = {0, Y_WIDTH / 2, 0};
+	t_coordinate	z_axis = {0, -(Y_WIDTH / 3), 0};
+	t_coordinate	origin = {0, 0, 0};
 
-	// put_line(&img, translate(origin, radian), translate(x_axis, radian), 0x00FF0000);
-	put_line(&img, translate(origin), translate(x_axis), 0x00FF0000);
-	// put_line(&img, translate(origin, radian), translate(y_axis, radian), 0x0000FF00);
+	// put_line(&img, origin, x_axis, 0x00FF0000);
+	// put_line(&img, origin, y_axis, 0x0000FF00);
+	// put_line(&img, origin, z_axis, 0x000000FF);
+
+	put_line(&img, translate((t_coordinate){0, 0, 0}), translate(x_axis), 0x00FF0000);
 	put_line(&img, translate(origin), translate(y_axis), 0x0000FF00);
 	put_line(&img, origin, z_axis, 0x000000FF);
 }
@@ -73,62 +62,14 @@ void	put_axis(t_data img)
 void	put_cube(t_data img)
 {
 	// 立方体の各座標
-	t_coordinate	a;
-	a.x = 0;
-	a.y = 0;
-	a.z = 0;
-
-	t_coordinate	b;
-	b.x = 100;
-	b.y = 0;
-	b.z = 0;
-
-	t_coordinate	c;
-	c.x = 0;
-	c.y = 100;
-	c.z = 0;
-
-	t_coordinate	d;
-	d.x = 100;
-	d.y = 100;
-	d.z = 0;
-
-	t_coordinate	e;
-	e.x = 0;
-	e.y = 0;
-	e.z = 100;
-
-	t_coordinate	f;
-	f.x = 100;
-	f.y = 0;
-	f.z = 100;
-
-	t_coordinate	g;
-	g.x = 0;
-	g.y = 100;
-	g.z = 100;
-
-	t_coordinate	h;
-	h.x = 100;
-	h.y = 100;
-	h.z = 100;
-
-/*
-	put_line(&img, translate(a, radian), translate(b, radian), 0x00FFFFFF);
-	put_line(&img, translate(a, radian), translate(c, radian), 0x00FFFFFF);
-	put_line(&img, translate(d, radian), translate(b, radian), 0x00FFFFFF);
-	put_line(&img, translate(d, radian), translate(c, radian), 0x00FFFFFF);
-
-	put_line(&img, translate(e, radian), translate(f, radian), 0x00FFFFFF);
-	put_line(&img, translate(e, radian), translate(g, radian), 0x00FFFFFF);
-	put_line(&img, translate(h, radian), translate(f, radian), 0x00FFFFFF);
-	put_line(&img, translate(h, radian), translate(g, radian), 0x00FFFFFF);
-
-	put_line(&img, translate(a, radian), translate(e, radian), 0x00FFFFFF);
-	put_line(&img, translate(b, radian), translate(f, radian), 0x00FFFFFF);
-	put_line(&img, translate(c, radian), translate(g, radian), 0x00FFFFFF);
-	put_line(&img, translate(d, radian), translate(h, radian), 0x00FFFFFF);
-*/
+	t_coordinate	a = {0, 0, 0};
+	t_coordinate	b = {100, 0, 0};
+	t_coordinate	c = {0, 100, 0};
+	t_coordinate	d = {100, 100, 0};
+	t_coordinate	e = {0, 0, 100};
+	t_coordinate	f = {100, 0, 100};
+	t_coordinate	g = {0, 100, 100};
+	t_coordinate	h = {100, 100, 100};
 
 	put_line(&img, translate(a), translate(b), 0x00FFFFFF);
 	put_line(&img, translate(a), translate(c), 0x00FFFFFF);
@@ -152,8 +93,6 @@ int	main(void)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	// double	radian;
-	// int	theta;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
@@ -161,9 +100,7 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
-	// put_axis(img, radian);
 	put_axis(img);
-	// put_cube(img, radian);
 	put_cube(img);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	// mlx_key_hook(&img, my_key_hook, &theta);
